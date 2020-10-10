0324af06e12b468ae86287555b81071c6fba5786
deekes <44477637+deekes@users.noreply.github.com>
2020-10-10 16:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter sendername");
+  	if(a== "sumathi"&&b=="deekeswar")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
